e85c15ebf9b9935f5933ac72c87f24e079cb4ad0
ViliusU <vilius.utaravicius@gmail.com>
2021-09-30 23:07:27 +0300

    1

--- src/main/java/com/example/sdaprojectincome_expenses_calculator/SdaProjectIncomeExpensesCalculatorApplication.java
+++ src/main/java/com/example/sdaprojectincome_expenses_calculator/SdaProjectIncomeExpensesCalculatorApplication.java
@@ -42,6 +42,5 @@ public class SdaProjectIncomeExpensesCalculatorApplication {
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(urlBasedCorsConfigurationSource);
-        
     }
 }
